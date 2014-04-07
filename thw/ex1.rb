496e7fecbcebde7e6bd397bfba996ec7927aa207
Harri Feng <harrifeng@gmail.com>
2014-04-07 16:37:41 +0800

    1st the hard way execise

---
+++ thw/ex1.rb
@@ -0,0 +1,44 @@
+puts "Hello World"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun"
+puts "Yay! Print"
+puts "I'd much rather you 'not'"
+puts 'I "said" do not touch  this.'
+
+puts "Extra Crredit"
+puts "1. Make your script pirnt another line"
+puts "------------------------------------------------------------>>"
+puts "another line"
+
+puts "2. Make your script print only one of the lines"
+puts "------------------------------------------------------------>>"
+puts "Yay! Print"
+
+puts "3. Put a #(octothorpe) character at the beginning of a line. What did it"
+puts "do? Try to find out what this character does"
+puts "------------------------------------------------------------>>"
+puts #"This octothorpe is comment sign"
+
+############################################################################
+# <===================OUTPUT===================>                           #
+# Hello World                                                              #
+# Hello Again                                                              #
+# I like typing this.                                                      #
+# This is fun                                                              #
+# Yay! Print                                                               #
+# I'd much rather you 'not'                                                #
+# I "said" do not touch  this.                                             #
+# Extra Crredit                                                            #
+# 1. Make your script pirnt another line                                   #
+# ------------------------------------------------------------>>           #
+# another line                                                             #
+# 2. Make your script print only one of the lines                          #
+# ------------------------------------------------------------>>           #
+# Yay! Print                                                               #
+# 3. Put a #(octothorpe) character at the beginning of a line. What did it #
+# do? Try to find out what this character does                             #
+# ------------------------------------------------------------>>           #
+#                                                                          #
+# => nil                                                                   #
+############################################################################
